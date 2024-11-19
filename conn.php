0a243ab2dd06f808208b2fcb7119afb69d153371
jempalacat <121996263+jempalacat@users.noreply.github.com>
2024-11-19 10:49:50 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,8 +1,8 @@
 <?php 
 $servername = "localhost";
-$username = "acut"; // replace with your MySQL username
-$password = "acut"; // replace with your MySQL password
-$database = "form_data"; // database name
+$username = "jem"; // replace with your MySQL username
+$password = "jem"; // replace with your MySQL password
+$database = "form"; // database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
